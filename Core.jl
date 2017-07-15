61b9cfe5a9113ae7c49ecdf6768077cdebe55908
adebesing <gregory.adebesin@gmail.com>
2017-07-15 16:56:30 +0100

    comp

--- Core.jl
+++ Core.jl
@@ -36,23 +36,3 @@ function comp(fnOne::Any, fnTwo::Any, fnThree::Any, fnRest...)
     comp(fnOne, fnTwo, fnThree)(fnThree(x))
   end
 end
-
-
-# (defn comp
-#   "Takes a set of functions and returns a fn that is the composition
-#   of those fns.  The returned fn takes a variable number of args,
-#   applies the rightmost of fns to the args, the next
-#   fn (right-to-left) to the result, etc."
-#   {:added "1.0"
-#    :static true}
-#   ([] identity)
-#   ([f] f)
-#   ([f g]
-#      (fn
-#        ([] (f (g)))
-#        ([x] (f (g x)))
-#        ([x y] (f (g x y)))
-#        ([x y z] (f (g x y z)))
-#        ([x y z & args] (f (apply g x y z args)))))
-#   ([f g & fs]
-#      (reduce1 comp (list* f g fs))))
