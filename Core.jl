4620f5817d234848ea02311064d9e3cd5bb8724a
adebesing <gregory.adebesin@gmail.com>
2017-07-15 16:55:29 +0100

    comp

--- Core.jl
+++ Core.jl
@@ -18,10 +18,41 @@ function comp(fnOne::Any, fnTwo::Any)
 end
 
 function comp(fnOne::Any, fnTwo::Any, fnThree::Any)
+  function f()
+    comp(fnOne, fnTwo)(fnThree)
+  end
+
   function f(x::Any)
     comp(fnOne, fnTwo)(fnThree(x))
   end
-  function f(x::Any, y...)
-    comp(fnOne, fnTwo)(fnThree(x))
+end
+
+
+function comp(fnOne::Any, fnTwo::Any, fnThree::Any, fnRest...)
+  function f()
+    comp(fnOne, fnTwo, fnThree)(fnThree)
+  end
+  function f(x::Any)
+    comp(fnOne, fnTwo, fnThree)(fnThree(x))
   end
 end
+
+
+# (defn comp
+#   "Takes a set of functions and returns a fn that is the composition
+#   of those fns.  The returned fn takes a variable number of args,
+#   applies the rightmost of fns to the args, the next
+#   fn (right-to-left) to the result, etc."
+#   {:added "1.0"
+#    :static true}
+#   ([] identity)
+#   ([f] f)
+#   ([f g]
+#      (fn
+#        ([] (f (g)))
+#        ([x] (f (g x)))
+#        ([x y] (f (g x y)))
+#        ([x y z] (f (g x y z)))
+#        ([x y z & args] (f (apply g x y z args)))))
+#   ([f g & fs]
+#      (reduce1 comp (list* f g fs))))
