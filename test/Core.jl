6901d2498a450184df60d463ba778c3b30c7677e
adebesing <gregory.adebesin@gmail.com>
2017-07-23 21:17:02 +0100

    added test

---
+++ test/Core.jl
@@ -0,0 +1,8 @@
+using Base.Test
+include(string(pwd(), "/api/Core.jl"))
+
+@test comp(string)(1) == "1"
+@test comp(string, float)(1) == "1.0"
+@test comp(string, (x) -> x + x)(2) == "4"
+@test comp(string)(0,1,2,3,4,5,6,7,8,9) == "0123456789"
+@test comp(parse, string)(0,1,2,3,4,5,6,7,8,9) == 123456789
