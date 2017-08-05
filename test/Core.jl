c3004a0e2963ba84709625dcc2adf0d8b84d4a30
Geem7n <gregory.adebesin@gmail.com>
2017-08-05 16:14:00 +0100

    str

--- test/Core.jl
+++ test/Core.jl
@@ -7,4 +7,5 @@ include(string(pwd(), "/api/Core.jl"))
 @test comp(string)(0,1,2,3,4,5,6,7,8,9) == "0123456789"
 @test comp(parse, string)(0,1,2,3,4,5,6,7,8,9) == 123456789
 @test comp(inc, inc, inc, inc, inc)(1) == 6
-#@test comp(inc, inc, inc, inc, str)(1) != "7" #TODO fix instance of object conversion failure
+#TODO fix instance of object conversion failure by creaing a universal type that all Simfoni fn's can be converted to and from
+#@test comp(inc, inc, inc, inc, str)(1) != "7"
